490c1ff9a07285a9e7c33ff311907e2a4522300a
David Négrier <d.negrier@thecodingmachine.com>
2016-07-04 17:59:35 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+TDBM DAOs for Mouf security
+===========================
+
+What is it?
+-----------
+
+This package contains a set of TDBM DAOs adding a basic user / role / right support for Mouf security.
+
+This package will create tables for users, roles and rights.
---
+++ src/TablesInstaller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mouf\Utils\Log\Psr;
+
+use Mouf\Database\Patcher\DatabasePatchInstaller;
+use Mouf\Installer\PackageInstallerInterface;
+use Mouf\MoufManager;
+
+/**
+ *
+ */
+class TablesInstaller implements PackageInstallerInterface {
+    /**
+     * (non-PHPdoc)
+     * @see \Mouf\Installer\PackageInstallerInterface::install()
+     */
+    public static function install(MoufManager $moufManager) {
+        DatabasePatchInstaller::registerPatch($moufManager,
+            "createUserRoleRightTablesPatch",
+            "This patch is added by the mouf/security.daos.tdbm package. It creates users, users_roles, roles, roles_rights tables.",
+            "vendor/mouf/security.daos.tdbm/database/up/users-roles-rights.sql", // SQL patch file, relative to ROOT_PATH
+            "vendor/mouf/security.daos.tdbm/database/down/users-roles-rights.sql"); // Optional SQL revert patch file, relative to ROOT_PATH
+
+
+        // Let's rewrite the MoufComponents.php file to save the component
+        $moufManager->rewriteMouf();
+    }
+}
