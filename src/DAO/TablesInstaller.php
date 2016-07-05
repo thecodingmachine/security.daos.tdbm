3a005daee61c1cf2c1edb8fbbe44205371c13fbf
David Négrier <d.negrier@thecodingmachine.com>
2016-07-05 10:09:36 +0200

    Upgrading minimum php version to 7.0 in Scrutinizer

--- src/DAO/TablesInstaller.php
+++ src/DAO/TablesInstaller.php
@@ -26,21 +26,33 @@ class TablesInstaller implements PackageInstallerInterface {
         $tdbmService = $moufManager->getInstanceDescriptor('tdbmService');
 
         // Let's create the instances.
-        $Mouf_Security_DAO_SecurityUserDao = InstallUtils::getOrCreateInstance('Mouf\\Security\\DAO\\SecurityUserDao', 'Mouf\\Security\\DAO\\SecurityUserDao', $moufManager);
+        $Mouf_Security_Rights_RightsRegistry = InstallUtils::getOrCreateInstance('Mouf\\Security\\Rights\\RightsRegistry', 'Mouf\\Security\\Rights\\RightsRegistry', $moufManager);
         $Mouf_Security_DAO_SecurityRightDao = InstallUtils::getOrCreateInstance('Mouf\\Security\\DAO\\SecurityRightDao', 'Mouf\\Security\\DAO\\SecurityRightDao', $moufManager);
+        $Mouf_Security_DAO_SecurityUserDao = InstallUtils::getOrCreateInstance('Mouf\\Security\\DAO\\SecurityUserDao', 'Mouf\\Security\\DAO\\SecurityUserDao', $moufManager);
 
         // Let's bind instances together.
-        if (!$Mouf_Security_DAO_SecurityUserDao->getConstructorArgumentProperty('tdbmService')->isValueSet()) {
-            $Mouf_Security_DAO_SecurityUserDao->getConstructorArgumentProperty('tdbmService')->setValue($tdbmService);
+        if (!$Mouf_Security_Rights_RightsRegistry->getConstructorArgumentProperty('rights')->isValueSet()) {
+            $Mouf_Security_Rights_RightsRegistry->getConstructorArgumentProperty('rights')->setValue(array());
         }
         if (!$Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('tdbmService')->isValueSet()) {
             $Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('tdbmService')->setValue($tdbmService);
         }
-        if (!$Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('rights')->isValueSet()) {
-            $Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('rights')->setValue(array());
+        if (!$Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('rightsRegistry')->isValueSet()) {
+            $Mouf_Security_DAO_SecurityRightDao->getConstructorArgumentProperty('rightsRegistry')->setValue($Mouf_Security_Rights_RightsRegistry);
+        }
+        if (!$Mouf_Security_DAO_SecurityUserDao->getConstructorArgumentProperty('tdbmService')->isValueSet()) {
+            $Mouf_Security_DAO_SecurityUserDao->getConstructorArgumentProperty('tdbmService')->setValue($tdbmService);
         }
 
-        // TODO: bind UserDao and RightsDao to userService and rightsService
+
+
+        // Lets bind UserDao and RightsDao to userService and rightsService
+        $rightsService = $moufManager->getInstanceDescriptor('rightsService');
+        $rightsService->getPublicFieldProperty('rightsDao')->setValue($Mouf_Security_DAO_SecurityRightDao);
+
+        $userService = $moufManager->getInstanceDescriptor('userService');
+        $userService->getPublicFieldProperty('userDao')->setValue($Mouf_Security_DAO_SecurityUserDao);
+
 
         // TODO: apply patch
         // TODO: regenerate DAOs.
